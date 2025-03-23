2e389fc7a0e39f72078975cde87d4e9cb87e3fbc
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-03-23 10:49:40 -0300

    Implemented King possible moves

--- src/main/java/com/gabrielnz/chess/ChessMatch.java
+++ src/main/java/com/gabrielnz/chess/ChessMatch.java
@@ -84,11 +84,11 @@ public class ChessMatch {
         placeNewPiece(8, 'b', new Knight(board, Color.BLACK));
         placeNewPiece(8, 'g', new Knight(board, Color.BLACK));
 
-
+        placeNewPiece(2, 'a', new Pawn(board, Color.WHITE));
         placeNewPiece(2, 'b', new Pawn(board, Color.WHITE));
         placeNewPiece(2, 'c', new Pawn(board, Color.WHITE));
         placeNewPiece(2, 'd', new Pawn(board, Color.WHITE));
-        placeNewPiece(2, 'e', new Pawn(board, Color.WHITE));
+        //placeNewPiece(2, 'e', new Pawn(board, Color.WHITE));
         placeNewPiece(2, 'f', new Pawn(board, Color.WHITE));
         placeNewPiece(2, 'g', new Pawn(board, Color.WHITE));
         placeNewPiece(2, 'h', new Pawn(board, Color.WHITE));
